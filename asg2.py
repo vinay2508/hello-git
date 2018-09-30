70855cdbd4e411e0bdffed0e317caa056436b9b9
vinay2508 <43571832+vinay2508@users.noreply.github.com>
2018-09-30 11:06:02 +0530

    Add files via upload

---
+++ asg2.py
@@ -0,0 +1,32 @@
+#Q.1-print something
+print('isro')
+#Q.2-concatinate strings
+str1='hello'
+str2='python'
+str1+str2
+#Q.3-Take the input of 3 variables x, y and z . Print their values on screen
+x=int(input('Enter value of x:'))
+print(x)
+y=int(input('Enter value of y:'))
+print(y)
+z=int(input('Enter value of z:'))
+print(z)
+#Q.4- Print "Let's get started" on screen.
+print('Let\'s get started')
+print("\"Let's get started\" ")
+#Q.5- Print the following values using placeholders.
+#s="Acadview"
+# course="Python"
+# fees=5000
+s = "Acadview"
+course = "Python"
+fees = 5000
+print("%s %s %d"%(s, course, fees))
+#Q.6- Find the area of circle
+# pi = 3.14
+# Take radius as input from user
+# Print the area of circle
+pi = 3.14
+r = float (input("Enter the radius:"))
+print (pi*r*r)
+print (pi*r**2)
